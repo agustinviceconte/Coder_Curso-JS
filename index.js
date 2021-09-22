71b1ad522df4ecb515a2b42ce94d0a1ec0c1eb70
agustinviceconte <agustinviceconte@outlook.com>
2021-09-22 07:22:43 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/*alert("Esto es un alert");
+console.log("Muestra de console log");**/
+
+let edad = 44;
+let nombre = "Agus";
+let esVerdad = true;
+let listas = [1,9,6];
+let objeto = {nombre: "1984", autor: "Orwell"};
+const IVA =0.21;
+
+
+
+let nombreIngresado = prompt("Ingrese su nombre");
+let suEdad =prompt("Ingrese su edad");
+
+console.log("nombre: " + nombreIngresado + ", edad: " + suEdad );
+
+let primerNumero =parseInt(prompt("Ingrese primer número"));
+let segundoNumero =parseInt(prompt("Ingrese segundo número"));
+let resultado = primerNumero + segundoNumero;
+
+console.log("La suma es: " + resultado);
+
+alert("Fin Clase 1 ")
+
